44b7175bc6118a5507b40af482b1b19689b9204f
Maria Mendoza Zafra <mmendozazafra@gmail.com>
2023-03-06 22:46:41 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const capital = document.querySelector('#capital');
+const tea = document.querySelector('#tea');
+const tiempo = document.querySelector('#tiempo');
+const calcular = document.querySelector('.calcular');
+const result = document.querySelector('.resultado');
+const result2 = document.querySelector('.resultado2');
+
+function calcInteresSimple(){
+    
+    let total = 0;
+    let cap = Number(capital.value);
+    let tasa = Number(tea.value);
+    let time = Number(tiempo.value);
+    simple = cap*(tasa/100)*time;
+    total = cap + simple;
+    result.innerHTML = "Los intereses ganados son S/. " + simple;
+    result2.innerHTML = "Al finalizar " + time + " años usted recibirá " + (simple + cap);
+}
+
